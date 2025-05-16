6c1835a6cc41ed8fef4a5b18e6946a934d8f4c95
Rushikeshkharekar1 <rushikeshkharekar17@gmail.com>
2025-05-17 00:16:49 +0530

    created project

---
+++ createSample/createSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace createSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
