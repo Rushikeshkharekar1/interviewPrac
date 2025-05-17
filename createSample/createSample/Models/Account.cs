ff91f5a760e9d88b8d2a1832588c5e025cba1627
Rushikeshkharekar1 <rushikeshkharekar17@gmail.com>
2025-05-18 05:00:43 +0530

    added connection

---
+++ createSample/createSample/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace createSample.Models;
+
+public partial class Account
+{
+    public int Empid { get; set; }
+
+    public string Empname { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ createSample/createSample/Models/IntPracContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace createSample.Models;
+
+public partial class IntPracContext : DbContext
+{
+    public IntPracContext()
+    {
+    }
+
+    public IntPracContext(DbContextOptions<IntPracContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Empid).HasName("PK__account__AF4CE865B72DE596");
+
+            entity.ToTable("account");
+
+            entity.Property(e => e.Empid).HasColumnName("empid");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Empname)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("empname");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
